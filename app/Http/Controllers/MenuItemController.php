9e6a4457cba553807e96d666b9f7a26c2412dc49
ronyjoseteranvilla <rony.18.32@gmail.com>
2019-10-10 09:38:53 +0200

    store menu/ / items, interate throw children

--- app/Http/Controllers/MenuItemController.php
+++ app/Http/Controllers/MenuItemController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Resources\MenuResource;
+use App\Menu;
+use App\Item;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class MenuItemController extends Controller
 {
@@ -12,9 +16,46 @@ class MenuItemController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $menu)
     {
-        //
+        $menu = Menu::find($menu);        
+        try {
+            $response = $this->interateMenuItems($request->all(), 0, '', $menu->id); 
+        } catch (\Throwable $th) {
+            return $th;
+        }
+        return response()->json($request->all());        
+    }
+
+    /**
+     * Interate the items array and inserts it and it's children
+     *
+     * @param [type] $data comment request->all data
+     * @param [type] $level comment level where the item is on the tree
+     * @param [type] $parent_id comment item parent id
+     * @param [type] $menu_id comment menu if where the item belongs
+     * @return boolean
+     */
+    public function interateMenuItems($data, $level, $parent_id, $menu_id): bool
+    {
+        $aux_parentID = '';
+        foreach ($data as $index1 => $node) {
+            foreach ($node as $index => $value) {
+                if($index === "children"){
+                    $val = $this->interateMenuItems($value, $level + 1, $aux_parentID, $menu_id);
+                }else if($index=='field'){
+                    $item = new Item();
+                    $item->field = $node['field'];
+                    $item->menu_id = $menu_id;
+                    $item->level = $level;
+                    $item->parent_id = ($parent_id != '')?$parent_id: null;
+                    $item->save(); 
+                    $aux_parentID = $item->id;                   
+                }
+            }
+        }
+
+        return true;
     }
 
     /**
--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable = [
+        'field',
+        'menu_id',
+        'parent_id',
+        'level'
+    ];
 }
